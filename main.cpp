e53de4d5de5a639f1f4b3685d6a600f316d830d8
Filip Žitný <filipzitny@gmail.com>
2023-10-21 13:24:55 +0200

    user_interface

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 #include "hex_to_decimal.h"
 
 int main() {
+    int input;
+    std::cout << "If you want to use decimal to hex converter, type 1. If you want to use hex to decimal converter, type 2." << std::endl;
+    std::cin >> input;
+
+    if(input == 1){
+        DecimalToHex converter;
+    }
+    else if(input == 2){
+        HexToDecimal converter;
+    }
+    else{
+        std::cout << "Wrong input." << std::endl;
+    }
+
+
     // input string hex number
     // convert it
     // display int decimal number
