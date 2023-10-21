ff92c969cd02535d19ffee4bf6e4ca7cae42c95b
Filip Žitný <filipzitny@gmail.com>
2023-10-21 13:05:34 +0200

    structure

--- decimal_to_hex.h
+++ decimal_to_hex.h
@@ -1,12 +1,13 @@
-//
-// Created by Filip Žitný on 21/10/2023.
-//
-
 #ifndef DEC_TO_HEX_DECIMAL_TO_HEX_H
 #define DEC_TO_HEX_DECIMAL_TO_HEX_H
-
+include <iostream>
 
 class DecimalToHex {
+
+public:
+    int readInput();
+    void convert(int input);
+    void print(std::string input);
 
 };
 
